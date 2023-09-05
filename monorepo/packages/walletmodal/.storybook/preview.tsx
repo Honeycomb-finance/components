4e8d80a50460f970e9db1f0fad794d631c67ac76
Pedro <pedroantonioppms@gmail.com>
2023-09-04 21:40:25 -0400

    fix: storybook

--- monorepo/packages/walletmodal/.storybook/preview.tsx
+++ monorepo/packages/walletmodal/.storybook/preview.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Preview } from '@storybook/react';
-import { ThemeProvider } from '@pangolindex/shared';
+import { useWeb3React as useWeb3ReactCore } from '@web3-react/core';
+import { HoneycombProvider } from '@pangolindex/honeycomb-provider';
+import { NetworkContextName } from '@pangolindex/shared';
+import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core';
+import getLibrary from '../src/utils/getLibrary';
+import { injected } from '@pangolindex/wallet-connectors';
+
+const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
 
 const InternalProvider = ({ children, theme }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  const { library, chainId, account } = useWeb3ReactCore();
+
+  return (
+    <HoneycombProvider library={library} chainId={chainId} account={account ?? undefined} theme={theme}>
+      {children}
+    </HoneycombProvider>
+  );
 };
 
 export const decorators = [
   (Story, metadata) => {
     const theme = metadata.args.theme ?? undefined;
     return (
-      <InternalProvider theme={theme}>
-        <Story />
-      </InternalProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Web3ProviderNetwork getLibrary={getLibrary}>
+          <InternalProvider theme={theme}>
+            <Story />
+          </InternalProvider>
+        </Web3ProviderNetwork>
+      </Web3ReactProvider>
     );
   },
 ];
