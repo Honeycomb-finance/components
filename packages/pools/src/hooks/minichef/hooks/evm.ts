615bcb6c1576ba9cf80552ef4ae80140c25c53d3
Sarju Hansaliya <sarju.hansaliya@gmail.com>
2023-12-20 18:21:49 +0530

    fix: formate file

--- packages/pools/src/hooks/minichef/hooks/evm.ts
+++ packages/pools/src/hooks/minichef/hooks/evm.ts
@@ -597,11 +597,8 @@ export const useGetMinichefStakingInfosViaSubgraph = (): MinichefStakingInfo[] =
         .div(ONE_TOKEN.toString());
       const totalStakedAmount = new TokenAmount(lpToken, minichefTvl.toString() || JSBI.BigInt(0));
       const totalStakedInUsd = new TokenAmount(lpToken, finalStakedValueInUSD.toString() || JSBI.BigInt(0));
-      
-      const stakedAmount = new TokenAmount(
-        lpToken,
-        farm?.farmingPositions?.[0]?.stakedTokenBalance?.toString() ?? '0',
-      );
+
+      const stakedAmount = new TokenAmount(lpToken, farm?.farmingPositions?.[0]?.stakedTokenBalance?.toString() ?? '0');
       const earnedAmount = new TokenAmount(
         png,
         JSBI.BigInt(userPendingRewardState?.result?.['pending']?.toString() ?? 0),
